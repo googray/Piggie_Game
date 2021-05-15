c38d0e92f2b4c299e6ad5d92ed5fc30ee56b3fa0
googray <83138023+googray@users.noreply.github.com>
2021-05-15 12:08:57 +0200

    update readme file, update script.js

--- script.js
+++ script.js
@@ -17,17 +17,19 @@ let scores, currentScore, activePlayer, playing;
 
 //Starting conditions
 //go to the func init()
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 //moi variant
 const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
   diceEl.classList.add("hidden");
   player0El.classList.remove("player--winner");
   player1El.classList.remove("player--winner");
